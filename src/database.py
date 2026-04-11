d4a34d1b883a6d49c7d93b69d3a27581045121fc
andoh <solomonkingsman@gmail.com>
2026-04-11 15:52:50 +0000

    add db session

--- src/database.py
+++ src/database.py
@@ -1,14 +1,15 @@
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from src.config import settings
 
-Base = declarative_base()
+
+class Base(DeclarativeBase):
+    pass
 
 
 engine = create_engine(url=settings.DATABASE_URL, echo=False)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(expire_on_commit=False, autoflush=False, bind=engine)
 
 
 def get_db():
